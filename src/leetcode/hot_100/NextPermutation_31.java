708ad44412ea66426c2ee76f7975bab02fb10631
jinhongcai <jinhongcai@jd.com>
2021-12-08 10:07:46 +0800

    hot_31

---
+++ src/leetcode/hot_100/NextPermutation_31.java
@@ -0,0 +1,21 @@
+package leetcode.hot_100;
+
+/**
+ * 实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。
+ * <p>
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * <p>
+ * 必须 原地 修改，只允许使用额外常数空间。
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/next-permutation
+ */
+public class NextPermutation_31 {
+
+    public void nextPermutation(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
