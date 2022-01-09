994c2b8d0f7f79c14096406913c6a027cad42cb4
jinhongcai <jinhongcai@jd.com>
2022-01-09 15:15:20 +0800

    1-9 PM commit 3

---
+++ src/leetcode/daily/medium/Daily_89_grayCode.java
@@ -0,0 +1,20 @@
+package leetcode.daily.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Daily_89_grayCode {
+
+    public List<Integer> grayCode(int n) {
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < 1 << n; i++) {
+            ret.add((i >> 1) ^ i);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(5 >> 1);
+    }
+
+}
