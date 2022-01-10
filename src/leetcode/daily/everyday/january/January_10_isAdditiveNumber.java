9afffa7ea103417cf602abe64f8b548f95ad657a
jinhongcai <jinhongcai@jd.com>
2022-01-10 11:58:01 +0800

    1-10 modified

--- src/leetcode/daily/everyday/january/January_10_isAdditiveNumber.java
+++ src/leetcode/daily/everyday/january/January_10_isAdditiveNumber.java
@@ -2,21 +2,46 @@ package leetcode.daily.everyday.january;
 
 public class January_10_isAdditiveNumber {
 
-    public static boolean isAdditiveNumber(String num) {
-        int one = num.charAt(0) - 48;
-        for (int i = 1; i < num.length() - 1; i++) {
-            if (num.charAt(i + 1) - 48 != one + num.charAt(i) - 48) {
+    public static boolean additiveNumber(String num, int carry) {
+        int[] dp = convert(num);
+        int one = dp[0];
+        for (int i = 0 + carry; i < dp.length; i += carry) {
+            String two = "";
+            for (int j = 0; j < carry; j++) {
+                two = two + dp[i - 1];
+            }
+            if (dp[i] != one + two) {
                 return false;
             }
-            one = num.charAt(i) - 48;
+            one = two;
+        }
+        return true;
+    }
+
+    public static boolean isAdditiveNumber(String num) {
+        int[] dp = convert(num);
+        int one = dp[0];
+        for (int i = 2; i < dp.length; i++) {
+            int two = dp[i - 1];
+            if (dp[i] != one + two) {
+                return false;
+            }
+            one = two;
         }
         return true;
     }
 
     public static void main(String[] args) {
-        String num = "112358";
+        String num = "199100199";
         System.out.println(isAdditiveNumber(num));
+    }
 
+    private static int[] convert(String num) {
         String[] split = num.split("");
+        int[] dp = new int[split.length];
+        for (int i = 0; i < split.length; i++) {
+            dp[i] = Integer.parseInt(split[i]);
+        }
+        return dp;
     }
 }
