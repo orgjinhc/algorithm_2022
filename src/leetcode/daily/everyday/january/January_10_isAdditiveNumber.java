ca450348ad73ecc87756daa03d12586ee61b5dae
jinhongcai <jinhongcai@jd.com>
2022-01-10 13:11:29 +0800

    1-10 modified

--- src/leetcode/daily/everyday/january/January_10_isAdditiveNumber.java
+++ src/leetcode/daily/everyday/january/January_10_isAdditiveNumber.java
@@ -2,46 +2,94 @@ package leetcode.daily.everyday.january;
 
 public class January_10_isAdditiveNumber {
 
-    public static boolean additiveNumber(String num, int carry) {
-        int[] dp = convert(num);
-        int one = dp[0];
-        for (int i = 0 + carry; i < dp.length; i += carry) {
-            String two = "";
-            for (int j = 0; j < carry; j++) {
-                two = two + dp[i - 1];
+    public static boolean isAdditiveNumber(String num) {
+        int N = num.length();
+        char firstStartValue = num.charAt(0);
+        //  穷举第二个数的开始位置, 等价于穷举第一个数的结束位置, 因为第一个数的开始位置已经固定
+        for (int secondStart = 1; secondStart < N - 1; ++secondStart) {
+            if (firstStartValue == '0' && secondStart != 1) {
+                break;
             }
-            if (dp[i] != one + two) {
-                return false;
+            //  穷举第二个数的结束位置
+            for (int secondEnd = secondStart; secondEnd < N - 1; ++secondEnd) {
+                if (num.charAt(secondStart) == '0' && secondStart != secondEnd) {
+                    break;
+                }
+                if (valid(secondStart, secondEnd, num)) {
+                    return true;
+                }
             }
-            one = two;
         }
-        return true;
+        return false;
     }
 
-    public static boolean isAdditiveNumber(String num) {
-        int[] dp = convert(num);
-        int one = dp[0];
-        for (int i = 2; i < dp.length; i++) {
-            int two = dp[i - 1];
-            if (dp[i] != one + two) {
-                return false;
+    /**
+     * 根据第二个数的开始和结束位置推导出第一个数的开始和结束位置
+     * 再根据前两个数的位置推导出第三个数的大小和位置
+     *
+     * @param secondStart
+     * @param secondEnd
+     * @param num
+     * @return
+     */
+    public static boolean valid(int secondStart, int secondEnd, String num) {
+        int N = num.length();
+        int firstStart = 0;
+        int firstEnd = secondStart - 1;
+        while (secondEnd <= N - 1) {
+            String third = stringAdd(num, firstStart, firstEnd, secondStart, secondEnd);
+
+            //  第三个数的开始和结束依次类推
+            int thirdStart = secondEnd + 1;
+            int thirdEnd = secondEnd + third.length();
+            if (thirdEnd >= N || !num.substring(thirdStart, thirdEnd + 1).equals(third)) {
+                break;
             }
-            one = two;
+            if (thirdEnd == N - 1) {
+                return true;
+            }
+            firstStart = secondStart;
+            firstEnd = secondEnd;
+            secondStart = thirdStart;
+            secondEnd = thirdEnd;
         }
-        return true;
+        return false;
+    }
+
+    /**
+     * 两数按位相加
+     *
+     * @param s
+     * @param firstStart
+     * @param firstEnd
+     * @param secondStart
+     * @param secondEnd
+     * @return
+     */
+    public static String stringAdd(String s, int firstStart, int firstEnd, int secondStart, int secondEnd) {
+        StringBuffer third = new StringBuffer();
+        int carry = 0;
+        int cur = 0;
+        while (firstEnd >= firstStart || secondEnd >= secondStart || carry != 0) {
+            cur = carry;
+            if (firstEnd >= firstStart) {
+                cur += s.charAt(firstEnd) - '0';
+                --firstEnd;
+            }
+            if (secondEnd >= secondStart) {
+                cur += s.charAt(secondEnd) - '0';
+                --secondEnd;
+            }
+            carry = cur / 10;
+            cur %= 10;
+            third.append((char) (cur + '0'));
+        }
+        third.reverse();
+        return third.toString();
     }
 
     public static void main(String[] args) {
         String num = "199100199";
         System.out.println(isAdditiveNumber(num));
-    }
-
-    private static int[] convert(String num) {
-        String[] split = num.split("");
-        int[] dp = new int[split.length];
-        for (int i = 0; i < split.length; i++) {
-            dp[i] = Integer.parseInt(split[i]);
-        }
-        return dp;
     }
 }
