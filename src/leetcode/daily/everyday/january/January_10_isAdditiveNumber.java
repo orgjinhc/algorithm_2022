270057d8c00990f53d80169487c342f2cb0079ca
jinhongcai <jinhongcai@jd.com>
2022-01-10 17:28:50 +0800

    1-10 PM commit 1

--- src/leetcode/daily/everyday/january/January_10_isAdditiveNumber.java
+++ src/leetcode/daily/everyday/january/January_10_isAdditiveNumber.java
@@ -2,46 +2,110 @@ package leetcode.daily.everyday.january;
 
 public class January_10_isAdditiveNumber {
 
-    public static boolean additiveNumber(String num, int carry) {
-        int[] dp = convert(num);
-        int one = dp[0];
-        for (int i = 0 + carry; i < dp.length; i += carry) {
-            String two = "";
-            for (int j = 0; j < carry; j++) {
-                two = two + dp[i - 1];
+    /**
+     * 此题使用三种技巧
+     * 1.暴力尝试, 三个数字进行相加逻辑, 只需要通过第二个数可以获得第一个数和第三个数(核心逻辑)
+     * 2.字符实现十进制相加
+     * 3.字符串比较和位置交换
+     *
+     * @param num
+     * @return
+     */
+    public static boolean isAdditiveNumber(String num) {
+        int N = num.length();
+        for (int secondStart = 1; secondStart < N - 1; secondStart++) {
+            //  只允许首位是0的情况, 其余情况不允许
+            if (num.charAt(0) == '0' && secondStart > 1) {
+                continue;
             }
-            if (dp[i] != one + two) {
+            //  暴力尝试第二个数的结束位置, 直到末尾位置
+            for (int secondEnd = secondStart; secondEnd < N - 1; secondEnd++) {
+                //  只允许首位是0的情况, 其余情况不允许
+                if (num.charAt(secondStart) == '0' && secondEnd > secondStart) {
+                    break;
+                }
+                if (isValid(num, secondStart, secondEnd)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 字符串比对
+     * firstStart = 0
+     * firstEnd = secondStart - 1
+     * secondStart = 递增
+     * secondEnd - 递增
+     * thirdStart = secondEnd + 1
+     * thirdEnd = secondEnd + length
+     *
+     * @param num
+     * @param secondStart
+     * @param secondEnd
+     * @return
+     */
+    private static boolean isValid(String num, int secondStart, int secondEnd) {
+        int N = num.length();
+        int firstStart = 0;
+        int firstEnd = secondStart - 1;
+        while (secondEnd < N) {
+            String third = obtainForThirdNumberByAdditiveNumber(num, firstStart, firstEnd, secondStart, secondEnd);
+            int thirdStart = secondEnd + 1;
+            int thirdEnd = secondEnd + third.length();
+
+            //  第三个数的结束位置已经越界 or 前两个数相加的结果和数字字符串的位置上的数不等
+            if (thirdEnd >= N || !num.substring(thirdStart, thirdEnd + 1).equals(third)) {
                 return false;
             }
-            one = two;
+            //  第三个数的位置正好是右边界情况, 直接返回true
+            if (thirdEnd == N - 1) {
+                return true;
+            }
+
+            //  继续验证下一个位置的数
+            firstStart = secondStart;
+            firstEnd = secondEnd;
+            secondStart = thirdStart;
+            secondEnd = thirdEnd;
         }
         return true;
     }
 
-    public static boolean isAdditiveNumber(String num) {
-        int[] dp = convert(num);
-        int one = dp[0];
-        for (int i = 2; i < dp.length; i++) {
-            int two = dp[i - 1];
-            if (dp[i] != one + two) {
-                return false;
+    /**
+     * 字符串相加实现
+     *
+     * @param num
+     * @param firstStart
+     * @param firstEnd
+     * @param secondStart
+     * @param secondEnd
+     * @return
+     */
+    private static String obtainForThirdNumberByAdditiveNumber(String num, int firstStart, int firstEnd, int secondStart, int secondEnd) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        while (firstEnd >= firstStart || secondEnd >= secondStart || carry != 0) {
+            int cur = 0;
+            if (firstEnd >= firstStart) {
+                cur += num.charAt(firstEnd) - '0';
+                firstEnd--;
             }
-            one = two;
+            if (secondEnd >= secondStart) {
+                cur += num.charAt(secondEnd) - '0';
+                secondEnd--;
+            }
+            int sum = cur + carry;
+            carry = sum / 10;
+            sb.append(sum % 10);
         }
-        return true;
+        sb.reverse();
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-        String num = "199100199";
+        String num = "12012122436";
         System.out.println(isAdditiveNumber(num));
-    }
-
-    private static int[] convert(String num) {
-        String[] split = num.split("");
-        int[] dp = new int[split.length];
-        for (int i = 0; i < split.length; i++) {
-            dp[i] = Integer.parseInt(split[i]);
-        }
-        return dp;
     }
 }
