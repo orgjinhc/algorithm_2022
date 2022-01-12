cd6a4e97a8f1cfd86aa3a11fbed1aa519cdb64d0
jinhongcai <jinhongcai@jd.com>
2022-01-12 11:08:42 +0800

    1-12 PM commit 1

--- src/leetcode/daily/medium/Daily_63_uniquePathsWithObstacles.java
+++ src/leetcode/daily/medium/Daily_63_uniquePathsWithObstacles.java
@@ -5,34 +5,33 @@ public class Daily_63_uniquePathsWithObstacles {
     public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int n = obstacleGrid.length;
         int m = obstacleGrid[0].length;
-        int[][] f = new int[n][m];
-        f[0][0] = obstacleGrid[0][0] == 0 ? 1 : 0;
-
-        for (int i = 0; i < m; ++i) {
-            if (obstacleGrid[i][0] == 0) {
-                f[i][0] = 1;
-            }
-        }
-        for (int j = 0; j < n; ++j) {
-            if (obstacleGrid[0][j] == 0) {
-                f[0][j] = 1;
-            }
-        }
-
-        for (int i = 1; i < n; ++i) {
-            for (int j = 1; j < m; ++j) {
+        int[] dp = new int[m];
+        dp[0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
                 if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
                     continue;
                 }
-                f[i][j] = f[i - 1][j] + f[i][j - 1];
+                if (j >= 1 && obstacleGrid[i][j - 1] == 0) {
+                    dp[j] += dp[j - 1];
+                }
             }
         }
-
-        return f[m - 1][n - 1];
+        return dp[m - 1];
     }
 
-
     public static void main(String[] args) {
-        System.out.println(uniquePathsWithObstacles(new int[][]{{0, 0, 0}, {1, 1, 1}, {0, 0, 0}}));
+        System.out.println(uniquePathsWithObstacles(new int[][]{
+                {0, 0, 0},
+                {0, 1, 1},
+                {0, 0, 0}
+        }));
+
+        /**
+         *  1, 1, 1
+         *  1, 0, 0
+         *  1, 1, 1
+         */
     }
 }
