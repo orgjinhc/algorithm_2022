ac7a429a98fc5870c52c01ece6455aaf2a5acf15
jinhongcai <jinhongcai@jd.com>
2022-01-22 14:30:22 +0800

    1-22 AM commit 1

--- src/leetcode/daily/everyday/january/January_22_removePalindromeSub.java
+++ src/leetcode/daily/everyday/january/January_22_removePalindromeSub.java
@@ -22,6 +22,24 @@ public class January_22_removePalindromeSub {
 
     public static void main(String[] args) {
         String s = "baabb";
-        System.out.println(removePalindromeSub(s));
+//        System.out.println(removePalindromeSub(s));
+
+        int[] nums = {2,2,1,9};
+        int target = 10;
+        System.out.println(purchasePlans(nums, target));
     }
+
+
+    public static int purchasePlans(int[] nums, int target) {
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] <= target) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
 }
