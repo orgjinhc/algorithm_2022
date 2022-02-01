9374e6d7f1dc8a1c47cab8342e50b916990e3df9
jinhongcai <jinhongcai@jd.com>
2022-02-01 19:23:34 +0800

    2-1 PM commit 1

---
+++ src/leetcode/daily/everyday/february/February_1_longestNiceSubstring.java
@@ -0,0 +1,30 @@
+package leetcode.daily.everyday.february;
+
+public class February_1_longestNiceSubstring {
+
+    public static String longestNiceSubstring(String s) {
+        int n = s.length();
+        int maxPos = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; ++i) {
+            int lower = 0;
+            int upper = 0;
+            for (int j = i; j < n; ++j) {
+                if (Character.isLowerCase(s.charAt(j))) {
+                    lower |= 1 << (s.charAt(j) - 'a');
+                } else {
+                    upper |= 1 << (s.charAt(j) - 'A');
+                }
+                if (lower == upper && j - i + 1 > maxLen) {
+                    maxPos = i;
+                    maxLen = j - i + 1;
+                }
+            }
+        }
+        return s.substring(maxPos, maxPos + maxLen);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
