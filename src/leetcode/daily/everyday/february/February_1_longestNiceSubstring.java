18087529373ca683a5a1ae8a138f4e6cdafd5e11
jinhongcai <jinhongcai@jd.com>
2022-02-01 20:11:22 +0800

    2-1 PM commit 1

--- src/leetcode/daily/everyday/february/February_1_longestNiceSubstring.java
+++ src/leetcode/daily/everyday/february/February_1_longestNiceSubstring.java
@@ -1,27 +1,39 @@
 package leetcode.daily.everyday.february;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class February_1_longestNiceSubstring {
 
     public static String longestNiceSubstring(String s) {
         int n = s.length();
-        int maxPos = 0;
-        int maxLen = 0;
-        for (int i = 0; i < n; ++i) {
-            int lower = 0;
-            int upper = 0;
-            for (int j = i; j < n; ++j) {
-                if (Character.isLowerCase(s.charAt(j))) {
-                    lower |= 1 << (s.charAt(j) - 'a');
-                } else {
-                    upper |= 1 << (s.charAt(j) - 'A');
-                }
-                if (lower == upper && j - i + 1 > maxLen) {
-                    maxPos = i;
-                    maxLen = j - i + 1;
+        String ans = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                String substring = s.substring(i, j + 1);
+                if (j - i + 1 > ans.length() && check(substring)) {
+                    ans = substring;
                 }
             }
         }
-        return s.substring(maxPos, maxPos + maxLen);
+        return ans;
+    }
+
+    public static boolean check(String s) {
+        Set<Character> set = new HashSet<>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            set.add(c);
+        }
+
+        for (char c : chars) {
+            char a = Character.toLowerCase(c);
+            char b = Character.toUpperCase(c);
+            if (!set.contains(a) || !set.contains(b)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
