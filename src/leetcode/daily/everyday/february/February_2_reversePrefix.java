4eb70008564d0ed54174ba72b0041f8c7918d70d
jinhongcai <jinhongcai@jd.com>
2022-02-09 12:34:19 +0800

    2-2 PM commit 1

--- src/leetcode/daily/everyday/february/February_2_reversePrefix.java
+++ src/leetcode/daily/everyday/february/February_2_reversePrefix.java
@@ -15,7 +15,29 @@ public class February_2_reversePrefix {
         return ans + word.substring(index + 1);
     }
 
-    public static void main(String[] args) {
+    public static int minSum(int[][] matrix) {
+        return f(matrix, 0, 0, matrix.length, matrix[0].length);
+    }
 
+    /**
+     * 从index出发, 到目标i、j位置的最短路径和
+     *
+     * @param matrix
+     * @param index
+     * @param i
+     * @param j
+     * @return
+     */
+    public static int f(int[][] matrix, int index, int i, int j) {
+        if (index == i && index == j) {
+            return 0;
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {2, 4, 1}, {4, 6, 2}};
     }
 }
